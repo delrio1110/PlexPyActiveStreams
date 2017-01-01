7236909a3096a4d7a6e791fddc8a689ae7f534ea
Andrew Kidd <contact@andrewiankidd.co.uk>
2017-01-01 01:31:09 +0000

    Initial Commit

---
+++ PlexPyActiveStreams/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PlexPyActiveStreams
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPPIP = new System.Windows.Forms.TextBox();
+            this.lblPPIP = new System.Windows.Forms.Label();
+            this.lblPPAPI = new System.Windows.Forms.Label();
+            this.txtPPAPI = new System.Windows.Forms.TextBox();
+            this.lblPPOutput = new System.Windows.Forms.Label();
+            this.txtPPOutput = new System.Windows.Forms.TextBox();
+            this.btnPPSubmit = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtPPIP
+            // 
+            this.txtPPIP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPPIP.Location = new System.Drawing.Point(12, 25);
+            this.txtPPIP.Name = "txtPPIP";
+            this.txtPPIP.Size = new System.Drawing.Size(310, 20);
+            this.txtPPIP.TabIndex = 0;
+            this.txtPPIP.Text = "192.168.0.6:8181";
+            // 
+            // lblPPIP
+            // 
+            this.lblPPIP.AutoSize = true;
+            this.lblPPIP.Location = new System.Drawing.Point(12, 9);
+            this.lblPPIP.Name = "lblPPIP";
+            this.lblPPIP.Size = new System.Drawing.Size(52, 13);
+            this.lblPPIP.TabIndex = 1;
+            this.lblPPIP.Text = "PlexPy IP";
+            // 
+            // lblPPAPI
+            // 
+            this.lblPPAPI.AutoSize = true;
+            this.lblPPAPI.Location = new System.Drawing.Point(12, 48);
+            this.lblPPAPI.Name = "lblPPAPI";
+            this.lblPPAPI.Size = new System.Drawing.Size(80, 13);
+            this.lblPPAPI.TabIndex = 3;
+            this.lblPPAPI.Text = "PlexPy API Key";
+            // 
+            // txtPPAPI
+            // 
+            this.txtPPAPI.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPPAPI.Location = new System.Drawing.Point(12, 64);
+            this.txtPPAPI.Name = "txtPPAPI";
+            this.txtPPAPI.Size = new System.Drawing.Size(310, 20);
+            this.txtPPAPI.TabIndex = 2;
+            this.txtPPAPI.Text = "affbc33fb32c8ec3ebf98828326982cf";
+            // 
+            // lblPPOutput
+            // 
+            this.lblPPOutput.AutoSize = true;
+            this.lblPPOutput.Location = new System.Drawing.Point(9, 116);
+            this.lblPPOutput.Name = "lblPPOutput";
+            this.lblPPOutput.Size = new System.Drawing.Size(77, 13);
+            this.lblPPOutput.TabIndex = 7;
+            this.lblPPOutput.Text = "PlexPy Output:";
+            // 
+            // txtPPOutput
+            // 
+            this.txtPPOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPPOutput.Location = new System.Drawing.Point(12, 132);
+            this.txtPPOutput.Multiline = true;
+            this.txtPPOutput.Name = "txtPPOutput";
+            this.txtPPOutput.Size = new System.Drawing.Size(310, 117);
+            this.txtPPOutput.TabIndex = 6;
+            // 
+            // btnPPSubmit
+            // 
+            this.btnPPSubmit.Location = new System.Drawing.Point(12, 90);
+            this.btnPPSubmit.Name = "btnPPSubmit";
+            this.btnPPSubmit.Size = new System.Drawing.Size(310, 23);
+            this.btnPPSubmit.TabIndex = 8;
+            this.btnPPSubmit.Text = "Save / Go";
+            this.btnPPSubmit.UseVisualStyleBackColor = true;
+            this.btnPPSubmit.Click += new System.EventHandler(this.btnPPSubmit_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 60000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 261);
+            this.Controls.Add(this.btnPPSubmit);
+            this.Controls.Add(this.lblPPOutput);
+            this.Controls.Add(this.txtPPOutput);
+            this.Controls.Add(this.lblPPAPI);
+            this.Controls.Add(this.txtPPAPI);
+            this.Controls.Add(this.lblPPIP);
+            this.Controls.Add(this.txtPPIP);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(350, 300);
+            this.Name = "Form1";
+            this.Text = "PlexPy Active Streams";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPPIP;
+        private System.Windows.Forms.Label lblPPIP;
+        private System.Windows.Forms.Label lblPPAPI;
+        private System.Windows.Forms.TextBox txtPPAPI;
+        private System.Windows.Forms.Label lblPPOutput;
+        private System.Windows.Forms.TextBox txtPPOutput;
+        private System.Windows.Forms.Button btnPPSubmit;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ PlexPyActiveStreams/Form1.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PlexPyActiveStreams
+{
+    public partial class Form1 : Form
+    {
+        iniHandler iniSettings;
+        static string activeStreams = "0";
+        public Form1()
+        {
+            InitializeComponent();
+            iniSettings = new iniHandler();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!System.IO.File.Exists("PlexPyActiveStreams.ini"))
+            {
+                updateIni();
+            }
+            else
+            {
+                txtPPIP.Text = iniSettings.Read("PPIP");
+                txtPPAPI.Text = iniSettings.Read("PPAPI");
+            }
+        }        
+
+        private void btnPPSubmit_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            updateIni();
+            getActiveStreams();
+            timer.Start();      
+        }
+
+        private void updateIni()
+        {
+            iniSettings.Write("PPIP", txtPPIP.Text);
+            iniSettings.Write("PPAPI", txtPPAPI.Text);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            getActiveStreams();
+        }
+
+        private void getActiveStreams()
+        {
+            using (var webClient = new System.Net.WebClient())
+            {
+                string url = "http://" + txtPPIP.Text + "/api/v2?apikey=" + txtPPAPI.Text + "&cmd=get_activity";
+                string json = webClient.DownloadString(url);
+                txtPPOutput.Text = json;
+                dynamic data = JObject.Parse(json);
+                activeStreams = data.response.data.stream_count;
+                notifyIcon.Icon = updateIcon(activeStreams);
+            }
+        }
+
+        private Icon updateIcon(string text)
+        {
+            Bitmap bitmap = new Bitmap(32, 32);
+
+            Icon icon = Properties.Resources.icon;
+            Font drawFont = new Font("Calibri", 16, FontStyle.Bold);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixel;
+            graphics.DrawIcon(icon, 0, 0);
+            graphics.DrawString(text, drawFont, drawBrush, 1, 2);
+            Icon createdIcon = Icon.FromHandle(bitmap.GetHicon());
+
+            drawFont.Dispose();
+            drawBrush.Dispose();
+            graphics.Dispose();
+            bitmap.Dispose();
+
+            return createdIcon;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon.Visible = true;
+                notifyIcon.ShowBalloonTip(500, "Minimized to tray!", "PlexPy will be polled every minute for new data", ToolTipIcon.Info);
+                this.Hide();
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon.Visible = false;
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+    }
+}
